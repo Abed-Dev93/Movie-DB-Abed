d2d4bca3f495474a81dcdd5fd2152fc84e5eea0d
Abdulrahman Ghassa <abdulrahman.ghassa93@gmail.com>
2023-09-22 16:28:43 +0300

    5- Set up the basis for CRUD

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const date = new Date()
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
+]
 
 app.get('/', (req, res) => {
     res.json('ok')
@@ -22,6 +28,22 @@ app.get('/hello/:id?', (req, res) =>{
 app.get('/search', (req, res) => {
     const queries = req.query.s
     queries ? res.status(200).json({status: 200, message: "ok", data: queries}) : res.status(500).json({status: 500, error: true, message: "you have to provide a search"})
+})
+
+app.get('/movies/create', (req, res) => {
+
+})
+
+app.get('/movies/read', (req, res) => {
+    res.status(200).json({status: 200, data: movies })
+})
+
+app.get('/movies/update', (req, res) => {
+    
+})
+
+app.get('/movies/delete', (req, res) => {
+    
 })
 
 app.listen(3000)
