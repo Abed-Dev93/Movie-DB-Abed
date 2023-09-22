8d68ee58bac3f63cc454530b2e2ac89fd7c26ca4
Abdulrahman Ghassa <abdulrahman.ghassa93@gmail.com>
2023-09-22 16:10:36 +0300

    3- Create an express simple API

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const date = new Date()
 
 app.get('/', (req, res) => {
     res.json('ok')
+})
+
+app.get('/test', (req, res) => {
+    res.status(200).json({status: 200, message: "ok"})
+})
+
+app.get('/time', (req, res) =>{
+    res.status(200).json({status: 200, message: `${date.getHours()}:${date.getMinutes()}`})
 })
 
 app.listen(3000)
