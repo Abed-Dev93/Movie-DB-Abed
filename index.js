ad5f37745a622c7807ecb6e6b78a59bc119890fd
Abdulrahman Ghassa <abdulrahman.ghassa93@gmail.com>
2023-09-22 16:25:13 +0300

    4- Let's complicate the API

--- index.js
+++ index.js
@@ -14,4 +14,14 @@ app.get('/time', (req, res) =>{
     res.status(200).json({status: 200, message: `${date.getHours()}:${date.getMinutes()}`})
 })
 
+app.get('/hello/:id?', (req, res) =>{
+    const { id } = req.params
+    id ? res.status(200).json({status: 200, message: `Hello, ${id}`}) : res.status(200).json({status: 200, message: 'Hello'})
+})
+
+app.get('/search', (req, res) => {
+    const queries = req.query.s
+    queries ? res.status(200).json({status: 200, message: "ok", data: queries}) : res.status(500).json({status: 500, error: true, message: "you have to provide a search"})
+})
+
 app.listen(3000)
